175c65b726bf450f8c12b132c4ce038a5258ac3c
mohaimen khalid <mohaimen707@gmail.com>
2020-02-20 20:10:44 +0600

    Ai stofware main page design

--- resources/views/ai_software/index.blade.php
+++ resources/views/ai_software/index.blade.php
@@ -1,14 +1,274 @@
 @extends('layouts.master')
 
-@section('title')
-List of Artificial Intelligence Software
+@section('title') List of Ai Software
 @endsection
 
 @section('content')
     @include('includes.header')
-    <div class="container">
-        <h3 class="ai-software-header">Artificial Intelligence Softwares</h3>
-       <div class="row">
+    <section class="highlight">
+        <div class="container">
+            <div class="row">
+                <div class="col col-sm-12 col-md-12 col-lg-12 ai-software-head">
+                    <h3>Find Alternative Software</h3>
+                    <h5>Currently, 1200 software added.</h5>
+                </div>
+                <div align="center" class="col-sm-12 col-md-12 col-lg-12 text-center d-flex justify-content-center align-items-center">
+                    <div class="col-md-8">
+                        <form>
+                            <input type="text" class="form-control p-2" placeholder="Search Your Software...">
+                            <button class="btn btn-danger mt-2">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="feature-software">
+        <div class="container">
+            <div class="box">
+               <div class="row box-title">
+                   <div class="col-md-12">
+                       <h2>Feature Software</h2>
+                   </div>
+               </div>
+                <div class="row box-div">
+                   <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                       <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                       <div><a class="box-image-title">Title</a></div>
+                   </div>
+                   <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                       <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                       <div><a class="box-image-title">Title</a></div>
+                   </div>
+                   <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                       <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                       <div><a class="box-image-title">Title</a></div>
+                   </div>
+                   <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                       <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                       <div><a class="box-image-title">Title</a></div>
+                   </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+               </div>
+           </div>
+        </div>
+    </section>
+    <section class="recently-added-software">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7 col-sm-12 col-lg-7">
+                    <div class="box">
+                        <div class="box-title">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <h2>Recently Added Software</h2>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <div class="row recently-added-software-item">
+                                <div class="col-md-2">
+                                    <img width="140" src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                                </div>
+                                <div class="col-md-10">
+                                    <div><a class="box-image-title">Title</a></div>
+                                    <p>Vidalytics is a video platform for marketers. It hosts, streams and plays your videos. It also comes wit... <a href="">read more</a></p>
+                                </div>
+                            </div>
+                            <div class="row recently-added-software-item">
+                                <div class="col-md-2">
+                                    <img width="140" src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                                </div>
+                                <div class="col-md-10">
+                                    <div><a class="box-image-title">Title</a></div>
+                                    <p>Vidalytics is a video platform for marketers. It hosts, streams and plays your videos. It also comes wit... <a href="">read more</a></p>
+                                </div>
+                            </div>
+                            <div class="row recently-added-software-item">
+                                <div class="col-md-2">
+                                    <img width="140" src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                                </div>
+                                <div class="col-md-10">
+                                    <div><a class="box-image-title">Title</a></div>
+                                    <p>Vidalytics is a video platform for marketers. It hosts, streams and plays your videos. It also comes wit... <a href="">read more</a></p>
+                                </div>
+                            </div>
+                            <div class="row recently-added-software-item">
+                                <div class="col-md-2">
+                                    <img width="140" src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                                </div>
+                                <div class="col-md-10">
+                                    <div><a class="box-image-title">Title</a></div>
+                                    <p>Vidalytics is a video platform for marketers. It hosts, streams and plays your videos. It also comes wit... <a href="">read more</a></p>
+                                </div>
+                            </div>
+                            <div class="row recently-added-software-item">
+                                <div class="col-md-2">
+                                    <img width="140" src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                                </div>
+                                <div class="col-md-10">
+                                    <div><a class="box-image-title">Title</a></div>
+                                    <p>Vidalytics is a video platform for marketers. It hosts, streams and plays your videos. It also comes wit... <a href="">read more</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-5 col-sm-12">For Ads</div>
+            </div>
+        </div>
+    </section>
+    <section class="feature-software">
+        <div class="container">
+            <div class="box">
+                <div class="row box-title">
+                    <div class="col-md-12">
+                        <h2 class="float-left">Artificial Intelligence Software</h2>
+                        <span class="float-right">
+                            <button class="btn btn-secondary">view more</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="row box-div">
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="feature-software">
+        <div class="container">
+            <div class="box">
+                <div class="row box-title">
+                    <div class="col-md-12">
+                        <h2 class="float-left">Voice Technology Software</h2>
+                        <span class="float-right">
+                            <button class="btn btn-secondary">view more</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="row box-div">
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="feature-software">
+        <div class="container">
+            <div class="box">
+                <div class="row box-title">
+                    <div class="col-md-12">
+                        <h2 class="float-left">Virtual Assistant Software</h2>
+                        <span class="float-right">
+                            <button class="btn btn-secondary">view more</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="row box-div">
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="feature-software">
+        <div class="container">
+            <div class="box">
+                <div class="row box-title">
+                    <div class="col-md-12">
+                        <h2 class="float-left">Image Recognition Software</h2>
+                        <span class="float-right">
+                            <button class="btn btn-secondary">view more</button>
+                        </span>
+                    </div>
+                </div>
+                <div class="row box-div">
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                    <div class="col-md-2 col-sm-6 col-lg-2 box-item">
+                        <img src="uploads/ai_software/image/2020/02/3m-health-care.jpg">
+                        <div><a class="box-image-title">Title</a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+       {{--<div class="row">
            <div class="col-md-9">
                @if($ai_softwares->count() < 1)
                    <div class="alert alert-primary" role="alert">
@@ -34,8 +294,7 @@ List of Artificial Intelligence Software
            <div class="col-md-3">
                @include('includes/google_ad')
            </div>
-       </div>
-    </div>
+       </div>--}}
     @include('includes.footer')
 @endsection
 
